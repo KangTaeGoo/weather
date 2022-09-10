3d3a51d8974c7b652d1a7384ca8e7c6fc6b61a58
KangTaeGoo <38896790+KangTaeGoo@users.noreply.github.com>
2022-09-11 03:39:12 +0900

    Add files via upload

---
+++ weather/src/main/java/zerobase/weather/controller/DiaryController.java
@@ -0,0 +1,63 @@
+package zerobase.weather.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+import zerobase.weather.domain.Diary;
+import zerobase.weather.error.InvalidDate;
+import zerobase.weather.service.DiaryService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class DiaryController {
+    private final DiaryService diaryService;
+
+    public DiaryController(DiaryService diaryService) {
+        this.diaryService = diaryService;
+    }
+
+    @ApiOperation(value = "일기 텍스토와 날씨를 이용해서 DB에 일기 저장", notes = "일기를 쓸 수 있는 API")
+    @PostMapping("/create/diary")
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                     LocalDate date, @RequestBody String text) {
+        diaryService.createDiary(date, text);
+
+    }
+    @ApiOperation("선택한 날짜의 모든 일기 데이터를 가져옵니다.")
+    @GetMapping("/read/diary")
+    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                   LocalDate date){
+//        if(date.isAfter(LocalDate.ofYearDay(2023, 1))){
+//            throw new InvalidDate();
+//        }
+        return diaryService.readDiary(date);
+
+    }
+    @ApiOperation("선택한 기간중의 모든 일기 데이터를 가져옵니다.")
+    @GetMapping("/read/diaries")
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                            @ApiParam(value = "조회할 기간의 첫번째 날"
+                                    , example = "2022-02-02")
+                            LocalDate startDate,
+                            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                            @ApiParam(value = "조회할 기간의 마지막 날"
+                            , example = "2022-02-02")LocalDate endDate){
+        return diaryService.readDiaries(startDate, endDate);
+    }
+    @ApiOperation("선택한 일기를 수정할 수 있습니다.")
+    @PutMapping("/update/diary")
+    void updateDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+                     @RequestBody String text ){
+        diaryService.updateDiary(date, text);
+    }
+    @ApiOperation("선택한 일기를 삭제합니다.")
+    @DeleteMapping("/delete/diary")
+    void deleteDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+        diaryService.deleteDiary(date);
+    }
+
+
+}
